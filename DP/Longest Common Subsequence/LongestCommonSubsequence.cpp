873b7f9e6d77a9259c8697a0528b201a2b1c1c98
Aayush Jain <aayushjn2@gmail.com>
2021-09-03 13:20:10 +0530

    longest common subsequence code segment added

--- DP/Longest Common Subsequence/LongestCommonSubsequence.cpp
+++ DP/Longest Common Subsequence/LongestCommonSubsequence.cpp
@@ -5,6 +5,23 @@ inline int max(int a,int b){
     return (a>b)?a:b;
 }
 
+int longestCommonSubsequence_(string a, string b) {
+    int x = a.length(), y = b.length();
+    int l[x+1][y+1];
+    bool row;
+    for(int i=0;i<=x;i++){
+        for(int j=0;j<=y;j++){
+            if(i==0 || j==0)
+                l[x][j]=0;
+            else if(a[i-1]==b[j-1])
+                l[x][j] = l[1-x][j-1]+1;
+            else
+                l[x][j] = max(l[1-x][j],l[x][j-1]);
+        }
+    }
+    return l[x][y];
+}
+
 int longestCommonSubsequence(string a, string b) {
     int x = a.length(), y = b.length();
     int l[2][y+1];
